eef63f0c97857675605824a0472b97ce4fe61054
Derek <derek.davison@dv-mobile.com>
2013-09-04 12:07:26 -0600

    update website

--- html/Package/dv/js/reddit.js.bak.js
+++ html/Package/dv/js/reddit.js.bak.js
@@ -3,16 +3,16 @@
   $.reddit = function(options, cb) {
     var posts = [], o = $.extend({}, $.reddit.defaults, options);
 
-    $.getJSON(o.api + '/' + o.subreddit + '?json=get_posts&count=' + o.limit, function(result){
-      $.each(result.posts, function(i, post){
+    $.getJSON(o.api + '/' + o.subreddit + '.json?jsonp=?&limit=' + o.limit, function(result){
+      $.each(result.data.children, function(i, post){
         // Create an absolute permalink containing the api url.
-        //post.data.permalink_absolute = o.api + post.data.permalink;
+        post.data.permalink_absolute = o.api + post.data.permalink;
 
         // Append to posts array.
-        posts.push(post.url);
+        posts.push(post.data);
 
         // Fire callback when finished parsing through posts.
-        if ( i + 1 == result.count ) {
+        if ( i + 1 == result.data.children.length ) {
           cb.call(self, posts);
         }
       });
@@ -20,9 +20,9 @@
   };
 
   $.reddit.defaults = {
-    api: 'http://dv-mobile.com/site',
+    api: 'http://reddit.com',
     subreddit: '',
-    limit: 6
+    limit: 20
   };
 
 })(jQuery);
