f22cc107a7ce258778b05a0c0cd84a4bdf1d0ebd
Derek <derek.davison@dv-mobile.com>
2013-11-26 14:21:59 -0700

    Re-create wordpress

--- html/Package/dv/Model/Activity.js
+++ html/Package/dv/Model/Activity.js
@@ -65,7 +65,7 @@
       }
       results = [];
       options = {
-        url: 'http://www.dv-mobile.com/site/api/get_category_posts/?category_slug=featured&count=10',
+        url: 'http://www.dv-mobile.com/I/api/get_category_posts/?category_slug=featured&count=10',
         type: 'GET',
         dataType: 'jsonp'
       };
